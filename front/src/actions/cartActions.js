167c23f6569ff196d660a7fe1323f6e9e8b520be
Hamza Tlili <68706294+hamzatli17@users.noreply.github.com>
2022-12-08 13:16:10 +0100

    Update cartActions.js

--- front/src/actions/cartActions.js
+++ front/src/actions/cartActions.js
@@ -7,7 +7,7 @@ const {
 
   } = require("../constants/cartConstants");
   export const addToCart = (id,qty) => async (dispatch,getState) => {
-    const res = await axios.get(`/api/products/${id}`);
+    const res = await axios.get(`https://e-commerce-service-b8r0.onrender.com/api/products/${id}`);
     dispatch({
         type: CART_ADD_ITEM,
         payload: {
