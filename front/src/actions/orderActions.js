2802c18e21ab545d1cff012f0e06ef36551cea11
Hamza Tlili <68706294+hamzatli17@users.noreply.github.com>
2022-12-08 13:17:11 +0100

    Update orderActions.js

--- front/src/actions/orderActions.js
+++ front/src/actions/orderActions.js
@@ -29,7 +29,7 @@ export const createOrder = (order) => async (dispatch, getState) => {
         Authorization: `Bearer ${userInfo.token}`,
       },
     }
-    const {data} = await axios.post("/api/orders/", order, config)
+    const {data} = await axios.post("https://e-commerce-service-b8r0.onrender.com/api/orders/", order, config)
 
     dispatch({
       type: ORDER_CREATE_SUCCESS,
@@ -61,7 +61,7 @@ export const getOrderDetails = (id) => async (dispatch, getState) => {
       },
     }
 
-    const { data } = await axios.get(`/api/orders/${id}`, config)
+    const { data } = await axios.get(`https://e-commerce-service-b8r0.onrender.com/api/orders/${id}`, config)
 
     dispatch({
       type: ORDER_DETAILS_SUCCESS,
@@ -102,7 +102,7 @@ export const payOrder = (orderId, paymentResult) => async (
     }
 
     const { data } = await axios.put(
-      `/api/orders/${orderId}/pay`,
+      `https://e-commerce-service-b8r0.onrender.com/api/orders/${orderId}/pay`,
       paymentResult,
       config
     )
