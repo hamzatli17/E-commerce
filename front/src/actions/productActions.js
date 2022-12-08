c9a31468ac1cc677109ad109c8bbc87d1c7256ad
Hamza Tlili <68706294+hamzatli17@users.noreply.github.com>
2022-12-08 13:17:35 +0100

    Update productActions.js

--- front/src/actions/productActions.js
+++ front/src/actions/productActions.js
@@ -12,7 +12,7 @@ const {
 export const listProducts = () => async (dispatch) => {
   try {
     dispatch({ type: PRODUCT_LIST_REQUEST });
-    const { data } = await axios.get("/api/products");
+    const { data } = await axios.get("https://e-commerce-service-b8r0.onrender.com/api/products");
     dispatch({
       type: PRODUCT_LIST_SUCCESS,
       payload: data,
@@ -30,7 +30,7 @@ export const listProducts = () => async (dispatch) => {
 export const listProductDetDETAILS = (id) => async (dispatch) => {
   try {
     dispatch({ type: PRODUCT_DETAILS_REQUEST });
-    const data = await axios.get(`/api/products/${id}`);
+    const data = await axios.get(`https://e-commerce-service-b8r0.onrender.com/api/products/${id}`);
     dispatch({
       type: PRODUCT_DETAILS_SUCCESS,
       payload: data.data,
