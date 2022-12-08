488607bbe5ffbcc92a3d8f404ade33dc5e591f2c
Hamza Tlili <68706294+hamzatli17@users.noreply.github.com>
2022-12-08 13:18:23 +0100

    Update userActions.js

--- front/src/actions/userActions.js
+++ front/src/actions/userActions.js
@@ -29,7 +29,7 @@ export const login = (email, password) => async (dispatch) => {
     }
 
     const { data } = await axios.post(
-      '/api/users/login',
+      'https://e-commerce-service-b8r0.onrender.com/api/users/login',
       { email, password },
       config
     )
@@ -69,7 +69,7 @@ export const register = (name, email, password) => async (dispatch) => {
     }
 
     const { data } = await axios.post(
-      '/api/users',
+      'https://e-commerce-service-b8r0.onrender.com/api/users',
       { name, email, password },
       config
     )
@@ -111,7 +111,7 @@ export const getUserDetails = (id) => async (dispatch, getState) => {
       },
     }
  
-    const { data } = await axios.get(`/api/users/${id}`, config)
+    const { data } = await axios.get(`https://e-commerce-service-b8r0.onrender.com/api/users/${id}`, config)
     dispatch({
       type: USER_DETAILS_SUCCESS,
       payload: data,
@@ -151,7 +151,7 @@ export const  updateUserProfile = (user) => async (dispatch, getState) => {
       },
     }
 
-    const { data } = await axios.put(`/api/users/profile`, user, config)
+    const { data } = await axios.put(`https://e-commerce-service-b8r0.onrender.com/api/users/profile`, user, config)
 
     dispatch({
       type: USER_UPDATE_PROFILE_SUCCESS,
